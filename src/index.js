00084f8a8175cc836e7b341317c6c559af89c4ea
waverlylandrith <143058072+waverlylandrith@users.noreply.github.com>
2024-11-14 23:17:56 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Render the main App component into the HTML
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
